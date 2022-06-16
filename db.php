31cf29dbe7183c59c6bc75db66fcc9c36f8e059c
walidhaddoury <47917672+walidhaddoury@users.noreply.github.com>
2022-06-16 11:32:26 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
 
 try {
-    $db = new PDO('mysql:host=188.34.153.174;port=3306;dbname=groupe6', 'groupe6', 'Super-Groupe6');
+    $db = new PDO('mysql:host=192.168.56.102;port=3306;dbname=groupe6', 'apache', 'root');
 }
 catch (Exception $e) {
     die('Erreur MySQL, maintenance en cours.' . $e->getMessage());
